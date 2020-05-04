20cad3a804174257a55d5422c2a76b7b4abf4f62
felps_mateos <felmateos@gmail.com>
2020-05-04 16:36:54 -0300

    update 1.0

---
+++ Joguito/Assets/scripts/playerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("eae jogo bosta");
+    }
+
+    public Rigidbody2D rb;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-0.05f, 0.05f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(0.05f, 0.05f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 5f);
+        }
+    }
+}
